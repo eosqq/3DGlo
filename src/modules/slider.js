f32802e360b18bc4f154640ac4b8e64e53ce11cc
eos_qq <on1x97@mail.ru>
2022-01-17 13:04:23 +0500

    Выполнение 23 задания

---
+++ src/modules/slider.js
@@ -0,0 +1,6 @@
+const slider = () => {
+    const sliderBlock = document.querySelector('.portfolio-content')
+    const slides = document.querySelector('.portfolio-content')
+}
+
+export default slider
