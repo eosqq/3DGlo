d70b639e9b9b2b6f30ed5ba9d67205310ad18481
quizzic <eosqqone@gmail.com>
2022-01-25 12:27:16 +0500

    25 задание

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,35 +1,29 @@
+import { animate } from "./helpers"
+
 const modal = () => {
     const buttons = document.querySelectorAll('.popup-btn')
     const modal = document.querySelector('.popup')
 
     let screenWidth = screen.width
-    
-    let count = 0;
-    let idAnimation;
 
     window.addEventListener('resize', () => {
         screenWidth = screen.width
     })
 
-    const animation = () => {
-        if (screenWidth >= 768) {
-            count++
-            idAnimation = requestAnimationFrame(animation)
-            if (count * 0.06 <= 1) {
-                modal.style.opacity = count * 0.06
-            } else {
-                cancelAnimationFrame(idAnimation)
-                count = 0
-            }
-        } else {
-            count = 0
-        }
-    }
-
     buttons.forEach((btn) => {
         btn.addEventListener('click', () => {
-            animation()
-            modal.style.display = 'block'
+            if (screenWidth >= 768) {
+                animate({
+                    duration: 300,
+                    timing(timeFraction) {
+                      return timeFraction;
+                    },
+                    draw(progress) {
+                        modal.style.opacity = progress
+                    },
+                  });
+            }
+            modal.style.display = 'block'   
         })
     })
 
